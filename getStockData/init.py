c59ae2cddd258c4b1318429aeb252869c1d1f0b3
gaozhansai <shenhuaxiyuan@163.com>
2021-01-12 16:39:33 +0800

    yaocai

--- getStockData/init.py
+++ getStockData/init.py
@@ -5,6 +5,12 @@ from src.aiDe import buyAiDe
 from src.fuYuan import buyFuYuan
 from src.tiger import buyTiger
 from src.dongCai import buyDongCai
+from src.yingLi import buyYingLi
+from src.zhangLe import buyZhangLe
+from src.jiaTou import buyJiaTou
+from src.dongFang import buyDongFang
+from src.yaoCai import buyYaoCai
+
 def buyStock():
     # buyZunJia('01490',True, 4000)
     # buyFuTu('01490',True, 4000)
@@ -12,7 +18,14 @@ def buyStock():
     # buyAiDe('01490',True, 4000)
     # buyFuYuan('01490',True, 4000)
     # buyTiger('01490',True, 4000)
-    buyDongCai('01490',True, 4000)
+    # buyDongCai('01490',True, 4000)
+    # buyYingLi('01490',True, 4000)
+    # buyZhangLe('01490',True, 4000)
+    # buyJiaTou('01490',True, 4000)
+    # buyDongFang('01490',True, 4000)
+    buyYaoCai('01490',True, 4000)
+    
+    
 
 if __name__ == '__main__':
     buyStock()
--- getStockData/src/getPwdData.py
+++ getStockData/src/getPwdData.py
@@ -4,7 +4,19 @@ def getPwd(name):
             'account':'10408453',
             'logInPwd':'Wef19910926',
             'tradePwd':'324872',
-        }
+        },
+        'dongFang':{
+            'account':'100285572',
+            # 'logInPwd':'Wef19910926',
+            'tradePwd':'Wef19919',
+        },
+        'yaoCai':{
+            'account':'M396811P',
+            'logInPwd':'Wef1991926',
+            # 'tradePwd':'Wef19919',
+        },
+        
+        
     }
     result = initData[(name)]
 
